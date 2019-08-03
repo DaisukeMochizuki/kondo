5b4b860fa1f0fd05cd641075adf48cc37b322269
vorkorro14 <vladimir.litvinenko.1997@gmail.com>
2019-08-03 23:00:22 +0300

    adding ros stuff

--- vision/detectors.py
+++ vision/detectors.py
@@ -68,6 +68,11 @@ class Detector:
         pass
 
     def __init__(self, detector_filename):
+	self._cv_bridge = CvBridge()
+        self._sub = rospy.Subscriber('/usb_cam/image_raw', Image, self.callback, queue_size=1)
+        self.features_pub = rospy.Publisher('detector/features', Point, queue_size=1)
+        self.resulted_img = rospy.Publisher('detector/resulted_img', CompressedImage, queue_size=1)
+	
         with open (detector_filename) as f:
             data = json.load(f)
 
@@ -104,4 +109,39 @@ class Detector:
             self.stages.append (curr_state)
 
         return self.stages [-1]
+
+    def callback(self, image_msg):
+        str_num = 0
+        try:
+            frame = self._cv_bridge.imgmsg_to_cv2(image_msg, desired_encoding="passthrough")
+        except CvBridgeError as e:
+            print(e)
+
+        bbox_tl, bbox_br = self.detect(frame)
+
+        frame_with_bbox = cv2.rectangle (frame.copy (), bbox_tl, bbox_br, (255, 0, 0), 5)
+
+        cv2.imshow('cv_img', frame_with_bbox)
+        cv2.waitKey(2)
+
+        img_msg = CompressedImage()
+        img_msg.header.stamp = rospy.Time.now()
+        img_msg.format = "jpeg"
+        img_msg.data = np.array(cv2.imencode('.jpg', frame_with_bbox)[1]).tostring()
+        # Publish new image
+        self.resulted_img.publish(img_msg)
+
+        features_msg = Point(float(bbox_tl[0]), float(bbox_tl[1]), float(0))
+        self.features_pub.publish(features_msg)
+        
+        #stages = detector.get_stages ()
+	
+        #for i in range (2):
+        #    cv2.imshow (str (i), stages[i])
+	
+if __name__ == "__main__":
+    rospy.init_node('detector')
+    conf_file = rospy.get_param('~conf_file')
+    detector = Detector(conf_file)
+    rospy.spin()
 
